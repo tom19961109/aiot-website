8f3355591f798819c9f40810b077de1552d779ce
Neil.Tsai <neil.tsai@aie-tec.com>
2026-09-11 14:36:48 +0800

    補上 nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,10 @@ export default defineNuxtConfig({
     enabled: true
   },
 
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
+
   css: ['~/assets/css/main.css'],
 
   content: {
