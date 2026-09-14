fa07ec95f00b6c3f20035306ebd610be05c34893
Neil.Tsai <neil.tsai@aie-tec.com>
2026-09-14 17:22:05 +0800

    加入Google 分析 代碼

--- app/app.vue
+++ app/app.vue
@@ -13,3 +13,22 @@
     </UApp>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-4WLK91PLGM',
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-4WLK91PLGM');
+      `,
+    },
+  ],
+});
+</script>
