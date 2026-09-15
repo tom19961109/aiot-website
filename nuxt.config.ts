83438831893dbf788cf12d503ba5fa8588e1a50d
Neil.Tsai <neil.tsai@aie-tec.com>
2026-09-15 10:15:05 +0800

    加入 @nuxtjs/sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,40 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@vueuse/nuxt', '@nuxtjs/i18n', '@nuxt/content'],
+  modules: ['@nuxt/ui', '@vueuse/nuxt', '@nuxtjs/i18n', '@nuxt/content', '@nuxtjs/sitemap'],
   css: ['~/assets/css/main.css'],
+
+  site: {
+    url: 'https://www.aie-tec.com.tw',
+    name: '安智聯科技'
+  },
 
   i18n: {
     defaultLocale: 'zh-TW',
+    strategy: 'prefix_except_default',
     locales: [
       {
         code: 'zh-TW',
         file: 'zh-TW.json',
-        name: '繁體中文'
+        name: '繁體中文',
+        language: 'zh-TW'
       },
       {
         code: 'en',
         file: 'en.json',
-        name: 'English'
+        name: 'English',
+        language: 'en'
       },
       {
         code: 'ja',
         file: 'ja.json',
-        name: '日本語'
+        name: '日本語',
+        language: 'ja'
       }
     ]
+  },
+
+  sitemap: {
+    autoLastmod: true
   }
 })
